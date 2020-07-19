ad3b2f0c6cf7c9e2a1d41af3d25bdc71db25f358
SayBGM <bgm537@saybgm.me>
2020-07-20 00:06:49 +0900

    feature: slack model 추가

---
+++ src/utils/model.ts
@@ -0,0 +1,13 @@
+export interface SlackParams {
+  token: string,
+  teamId: string,
+  teamDomain: string,
+  channelId: string,
+  channelName: string,
+  userId: string,
+  userName: string,
+  command: string,
+  text: string,
+  responseUrl: string,
+  triggerId: string,
+}
